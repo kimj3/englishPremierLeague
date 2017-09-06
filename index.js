509ea31c86d7edf57bc4ff3afc735f0519294c2c
Joon Kim <kimj3@carleton.edu>
2017-09-05 20:01:54 -0500

    added some data

--- index.js
+++ index.js
@@ -2,18 +2,7 @@
 const chalk = require('chalk')
 const pkg = require('./package.json')
 
-const nameError =
-`*******************************************************************
- You need to give your app a name.
-
- The package name
-
-    ${pkg.name}
-
-isn't valid. If you don't change it, things won't work right.
-
-Please change it in ${__dirname}/package.json
-********************************************************************`
+const nameError = pkg.name
 
 const reasonableName = /^[\w\-]+$/
 if (!reasonableName.test(pkg.name)) {
